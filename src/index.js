6223ca86805df09b298e0056dd9611ba653806f5
Sudharsan-6955 <sudharsan6955@gmail.com>
2025-10-01 07:59:34 +0530

    feat: Whitelist all require\l backend folder as per new .gitignore

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import '././assets/css/icofont.min.css';
-import '././assets/css/animate.css';
-import '././assets/css/style.min.css';
+import './assets/css/icofont.min.css';
+import './assets/css/animate.css';
+import './assets/css/style.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
